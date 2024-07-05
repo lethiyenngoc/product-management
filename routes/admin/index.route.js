22e1775108cb282d1ec30bddde9b8406443faf44
Le Thi Yen Ngoc <x.uyen050519@gmail.com>
2024-07-05 14:28:42 +0700

    Lam giao dien danh sach san pham

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -1,5 +1,7 @@
 const dashboardRoute = require("./dashboard.route");
+const productsdRoute = require("./product.route");
 
 module.exports.index = (app) => {
     app.use("/admin/dashboard", dashboardRoute);
+    app.use("/admin/products", productsdRoute);
 }
