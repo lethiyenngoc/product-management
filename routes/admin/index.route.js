309d4291956e479abd34717fc4e821e98d7ac72e
Le Thi Yen Ngoc <x.uyen050519@gmail.com>
2024-07-07 16:01:54 +0700

    Toi uu lai bien admin

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -1,7 +1,11 @@
 const dashboardRoute = require("./dashboard.route");
 const productsdRoute = require("./product.route");
+const systemConfig = require("../../config/system");
 
 module.exports.index = (app) => {
-    app.use("/admin/dashboard", dashboardRoute);
-    app.use("/admin/products", productsdRoute);
+    const path = `/${systemConfig.prefixAdmin}`;
+
+    app.use(`${path}/dashboard`, dashboardRoute);
+    
+    app.use(`${path}/products`, productsdRoute);
 }
