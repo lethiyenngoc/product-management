38ea0c4db1688d32967861f3fbf58d8659f3f1f3
Le Thi Yen Ngoc <x.uyen050519@gmail.com>
2024-07-08 13:58:22 +0700

    Tinh nang loc theo trang thai - FE

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -1,1 +1,21 @@
-console.log("OK");
+// Button Status
+const listButtonStatus = document.querySelectorAll("[button-status");
+if(listButtonStatus.length > 0) {
+    let url = new URL(window.location.href);
+
+    listButtonStatus.forEach(button => {
+        button.addEventListener("click", () => {
+            const status = button.getAttribute("button-status");
+            url.searchParams.set("status", status);
+            window.location.href = url.href;
+        });
+    });
+
+    // Thêm class active mặc định
+    const statusCurrent = url.searchParams.get("status") || "";
+    const buttonCurrent = document.querySelector(`[button-status="${statusCurrent}"]`);
+    if(buttonCurrent) {
+        buttonCurrent.classList.add("active");
+    }
+}
+// End Button Status
